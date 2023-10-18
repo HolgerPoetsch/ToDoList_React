53303229faddb26e2cc2cc53662403531023fdbe
Nora Westphal <tupfen@hotmail.de>
2023-10-18 16:50:11 +0200

    Array of Objects added, used in new component ToDoList to show tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
+import ToDoList from "./components/ToDoList"
+
+// Dieses Array an Objekten kann zum Testen verwendet werden.
+// Rick: Objekte hinzufügen
+// Holger: Objekte löschen
+// Nora: Objekte anzeigen und bearbeiten
+const myTestObjects = [
+  {
+    task: "Staubsaugen",
+    status: true
+  },
+  {
+    task: "Wäsche waschen",
+    status: true
+  },
+  {
+    task: "Spazieren gehen",
+    status: true
+  }
+]
+
 
 function App() {
   return (
-    <div class="container">
-      <div class="todo-app">
+    <div className="container">
+      <div className="todo-app">
         <h2>To-Do List</h2>
-          <div class="row">
-{/*Rick:    <input type="text" id="input-box" placeholder="Add your text">
+        <div className="row">
+          {/*Rick:    <input type="text" id="input-box" placeholder="Add your text">
             <button onclick="addTask()">Add</button>*/}
-            </div>
-
- {/*Nora    <ul id="list-container">
+        </div>
+        <ToDoList myObjects={myTestObjects}/>
+        {/*Nora    <ul id="list-container">
 
             </ul>*/}
       </div>
-{/*Holger <div id="todo-delete">
+      {/*Holger <div id="todo-delete">
           <button id="delete-button">Delete All</button>
           </div>*/}
     </div>
