5bcd6a36af66baa19164cfedbf9f004e87d730ec
Nora Westphal <tupfen@hotmail.de>
2023-10-21 21:56:45 +0200

    Checkboxen funktionstüchtig implementiert

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -5,7 +5,7 @@ export default function ToDoList({ myObjects, setMyObjects }) {
     return (
         <>
             <h3>My ToDos</h3>
-            <ul>
+            <ul id="list-container">
                 {myObjects.map((obj) => <ItemBuilder key={obj.task} obj={obj} myObjects={myObjects} setMyObjects={setMyObjects} />)}
             </ul>
         </>
@@ -14,11 +14,8 @@ export default function ToDoList({ myObjects, setMyObjects }) {
 
 function ItemBuilder({ obj, myObjects, setMyObjects }) {
 
-    // Das mit den Icons zu wechseln hat irgendwie noch nicht funktioniert :(
-    const [icons, setIcons] = useState(["\u{1F589}", "\u{00d7}"])
-
+    const [[editIcon, hideText], setEditState] = useState(["\u{1F589}", { "display": "inline" }])
     const [inputEdit, setInputEdit] = useState(null);
-    const [hideText, setHideText] = useState({"display": "inline"})
 
     function deleteListItem(obj, myObjects, setMyObjects) {
         setMyObjects(myObjects.filter((element) => element.task !== obj.task))
@@ -30,24 +27,30 @@ function ItemBuilder({ obj, myObjects, setMyObjects }) {
                 return element;
             } else if (inputEdit) {
                 element.task = inputEdit;
-                setHideText({"display":"inline"})
+                setEditState(["\u{1F589}", { "display": "inline" }])
                 setInputEdit(null)
-                setIcons(["\u{1F589}", "\u{00d7}"])
                 return element
             } else {
-                setHideText({"display":"none"})
+                setEditState(["\u{1F5AB}", { "display": "none" }])
                 setInputEdit(element.task)
-                setIcons(["\u{1F5AB}", "\u{00d7}"])
                 return element;
             }
         })
         setMyObjects(myNewObject)
     }
 
+    function checkListItem() {
+        const myNewObject = myObjects.map((element) => {
+            element.id == obj.id ? obj.status = !obj.status : obj.status = obj.status;
+            return element;
+        })
+        setMyObjects(myNewObject)
+    }
+
     return (
-        <li>
+        <li className={obj.status ? "checked" : ""} onClick={(e) => e.target.tagName=="LI" && checkListItem()}>
             <input value={inputEdit} type={!inputEdit && "hidden"} onChange={(e) => setInputEdit(e.target.value)} /><p style={hideText}>{obj.task}</p>
-            <span className="editSpan" onClick={() => editListItem(obj, myObjects, setMyObjects)} >{icons[0]}</span>
-            <span onClick={() => deleteListItem(obj, myObjects, setMyObjects)}>{icons[1]}</span>
+            <span className="editSpan" onClick={() => editListItem(obj, myObjects, setMyObjects)} >{editIcon}</span>
+            <span onClick={() => deleteListItem(obj, myObjects, setMyObjects)}>{"\u{00d7}"}</span>
         </li>)
 } 
