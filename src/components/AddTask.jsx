b5a3d9cff1933d8d767a4737fc10ebc7ef76d4db
Rick <rlochner@gmail.com>
2023-10-23 09:42:10 +0200

    clear addfield

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -12,11 +12,12 @@ function AddTask({ myObjects, setMyObjects }) {
     const newId = crypto.randomUUID()
     setMyObjects((prev) => [...prev, {id : newId, task : textinput, status: false}]);
     localStorage.setItem([newId], JSON.stringify({ task: textinput, status: false }))
+    setTextinput(""); 
   };
 
   return (
     <>
-      <input type="text" placeholder="Add your task..." onChange={changeText} />
+      <input value={textinput} type="text" placeholder="Add your task..." onChange={changeText} />
       <button type="submit" onClick={handleOnClick}>
         Add
       </button>
